f06504827cc27ac08c368dce93421bd2fa94d560
BioMaRu <Bio.Color.Palette@gmail.com>
2018-12-23 01:22:20 +0700

    update responsive

--- doc/components/Navbar.vue
+++ doc/components/Navbar.vue
@@ -2,12 +2,12 @@
 	<nav class="lo-container navbar">
 		<div class="navbar-container">
 			<nuxt-link to="/" class="_dp-f _alit-ct">
-				<img src="~assets/icon/turbine.svg" alt="fastwork" draggable="false" class="_h-48px">
-				<div class="_fs-4 _fw-600 _cl-secondary _mgl-12px" style="letter-spacing: -1px">
+				<img src="~assets/icon/turbine.svg" alt="fastwork" draggable="false" class="_h-32px">
+				<div class="_fs-5 _fw-600 _cl-secondary _mgl-12px" style="letter-spacing: -1px">
 					Fastwork <span class="_fw-300">| engineer</span>
 				</div>
 			</nuxt-link>
-			<div>
+			<div class="_dp-n _dp-b-md">
 				<ul class="_dp-f _alit-ct _lst-n _mg-0px _pd-0px">
 					<li class="_mgl-32px"><a href="#" class="tb-link -primary _fw-600 _fs-5">Turbine UI</a></li>
 					<li class="_mgl-32px"><a href="#" class="tb-link -primary _fw-600 _fs-5">Articles</a></li>
@@ -18,6 +18,9 @@
 						</a>
 					</li>
 				</ul>
+			</div>
+			<div class="_dp-n-md">
+				=
 			</div>
 		</div>
 	</nav>
